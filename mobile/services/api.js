985c7dc7aeb1cf274fd548a1d03c24801d3459aa
Bagus Rahmat hidayat <rahmatbagus5656@gmail.com>
2026-05-23 22:40:05 +0700

    Configure mobile APK build

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,13 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Production backend Render
 const API_BASE_URL = 'https://myskillapp.onrender.com/api';
-
-// Local development examples:
-// iOS Simulator: const API_BASE_URL = 'http://localhost:8000/api';
-// Android Emulator: const API_BASE_URL = 'http://10.0.2.2:8000/api';
-// Real device: const API_BASE_URL = 'http://IP_LAPTOP_ANDA:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
