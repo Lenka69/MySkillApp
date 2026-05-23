24683d98e33a59c71cf8bfe26e314a0011cc2c3f
Bagus Rahmat hidayat <rahmatbagus5656@gmail.com>
2026-05-23 19:47:26 +0700

    Update api.js dan tambah konfigurasi vercel

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
 });
 
-// Otomatis menyisipkan token ke setiap request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
